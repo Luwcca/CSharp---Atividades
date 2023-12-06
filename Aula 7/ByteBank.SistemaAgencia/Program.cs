d0a8a0d2a9381bb4b5fcba28db0fd1185022f3df
Lucca <lucca.a.gioppo@gmail.com>
2023-12-05 21:41:22 -0300

    Aula 7 - arrays

---
+++ Aula 7/ByteBank.SistemaAgencia/Program.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ByteBank.Modelos;
+using ByteBank.Modelos.Funcionarios;
+
+namespace ByteBank.SistemaAgencia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ListaDeObject listaDeIdades = new ListaDeObject();
+
+            listaDeIdades.Adicionar(10);
+            listaDeIdades.Adicionar(5);
+            listaDeIdades.Adicionar(4);
+            listaDeIdades.AdicionarVarios(16, 23, 60);
+
+            for(int i = 0; i< listaDeIdades.Tamanho; i++)
+            {
+                int idade = (int)listaDeIdades[i];
+                Console.WriteLine($"Idade do indice {i} = {idade}");
+
+            }
+
+
+            Console.ReadLine();
+        }
+
+        static void TstaListaContaCorrente()
+        {
+            //ListaDeContaCorrente lista = new ListaDeContaCorrente();
+            ListaDeContaCorrente lista = new ListaDeContaCorrente();
+
+            ContaCorrente contaDoGui = new ContaCorrente(546, 111111);
+
+            ContaCorrente[] contas = new ContaCorrente[]
+            {
+                contaDoGui,
+                new ContaCorrente(874, 5679787)
+            };
+
+            lista.AdicionarVarios(contas);
+
+
+            lista.AdicionarVarios(
+                contaDoGui,
+                new ContaCorrente(874, 5679787),
+                new ContaCorrente(874, 5679787),
+                new ContaCorrente(874, 5679787),
+                new ContaCorrente(874, 5679787),
+                new ContaCorrente(874, 5679787),
+                new ContaCorrente(874, 5679787),
+                new ContaCorrente(874, 5679787)
+            );
+
+
+            //lista.EscreverListaNaTela();
+
+
+
+            //lista.Remover(contaDoGui);
+
+            //lista.EscreverListaNaTela();
+
+
+            for (int i = 0; i < lista.Tamanho; i++)
+            {
+                ContaCorrente itemAtual = lista[i];
+                Console.WriteLine($"Item na posição {i} = Conta {itemAtual.Numero}/{itemAtual.Agencia}");
+            }
+        }
+
+        static void TesteArrayInstacia()
+        {
+            ContaCorrente[] contas = new ContaCorrente[]
+           {
+                new ContaCorrente(874, 5679787),
+                new ContaCorrente(874, 4456668),
+                new ContaCorrente(874, 7781438)
+           };
+
+
+
+            for (int indice = 0; indice < contas.Length; indice++)
+            {
+                Console.WriteLine($"Conta {indice} {contas[indice].Numero}");
+            }
+
+
+        }
+
+
+
+        static void TestaArrayInt()
+        {
+
+            int[] idades = new int[5];
+
+            idades[0] = 15;
+            idades[1] = 28;
+            idades[2] = 35;
+            idades[3] = 50;
+            idades[4] = 28;
+
+            Console.WriteLine(idades.Length);
+
+
+            int acumulador = 0;
+            for (int i = 0; i < idades.Length; i++)
+            {
+                int idade = idades[i];
+
+                Console.WriteLine($"Acessando o array idades no índice {i}");
+                Console.WriteLine($"Valor de idades[{i}] = {idade}");
+
+                acumulador += idade;
+            }
+
+            int media = acumulador / idades.Length;
+            Console.WriteLine($"Média de idades = {media}");
+        }
+
+    }
+}
