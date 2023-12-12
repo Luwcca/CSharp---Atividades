8a76585bfb47b5cd5a4659dd150dd9b8ae853ee3
Lucca <lucca.a.gioppo@gmail.com>
2023-12-11 22:16:06 -0300

    Aula 9 - I/O e File Manip

---
+++ Aula 9 - IO com streams/ByteBankImportacaoExportacao/4_StreamBinario.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBankImportacaoExportacao
+{
+    partial class Program
+    {
+        static void EscritaBinaria()
+        {
+            using (var fs = new FileStream("contaCorrente.txt", FileMode.Create))
+            using (var escritor = new BinaryWriter(fs))//escreve em binário
+            {
+                escritor.Write(456);//número da Agência
+                escritor.Write(546544);//número da conta
+                escritor.Write(4000.50);//Saldo
+                escritor.Write("Gustavo Braga");
+            }
+        }
+
+        static void LeituraBinaria()
+        {
+            using (var fs = new FileStream("contaCorrente.txt", FileMode.Open))
+            using (var escritor = new BinaryReader(fs))
+            {
+                var agencia = escritor.ReadInt32();
+                var numeroConta = escritor.ReadInt32();
+                var saldo = escritor.ReadDouble();
+                var titular = escritor.ReadString();
+
+
+                Console.WriteLine($"{agencia}/{numeroConta} {titular} {saldo}");
+
+            }
+        }
+
+    }
+}
+
