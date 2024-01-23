50d7de2cec2878e7a538f578503e6d9ee27fae50
Lucca <lucca.a.gioppo@gmail.com>
2024-01-23 20:12:27 -0300

    Clean Architecture Essencial e APIs

--- WebApi/FilmesApi/FilmesApi/obj/Debug/net6.0/FilmesApi.AssemblyInfo.cs
+++ WebApi/FilmesApi/FilmesApi/obj/Debug/net6.0/FilmesApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FilmesApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+62bd6f4b9abbe3a135ca0b85e4988ec7dc60630e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("FilmesApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FilmesApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
